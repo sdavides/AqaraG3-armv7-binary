46e1eb82362f2479aee8638efe441196608c7a7f
sdavides <31100253+sdavides@users.noreply.github.com>
2024-12-11 21:59:28 +0100

    Create telnet_getprop_aqara.sh

---
+++ telnet_getprop_aqara.sh
@@ -0,0 +1,1 @@
+( sleep 3; printf "root\n"; sleep 1; printf "clear\n"; sleep 1; printf "agetprop full\n"; sleep 1; ) | telnet 192.168.1.4 | sed '1,7d' | grep -v '#'| sed -e 's/\]: \[/" \: \"/g' | sed -e 's/\[/"/g' | sed -e 's/\]/"/g' |sed 's/^/,/'|  sed -e '$a}}' |sed  '1s/^./{"0":{/' | sed 's/"{"/{"/' |  sed -e 's/\"\}\"/\"\}/g' | sed -e 's/\}\"/}/g' > ./getprop_aqara.js
